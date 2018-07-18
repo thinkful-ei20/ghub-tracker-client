6a2a4d45fa6f7c7d920e8aa808def139c544641c
Hiram Cruz <hiram0101@gmail.com>
2018-07-18 14:15:32 -0400

    changed fetched endpoint

--- src/actions/protected-data.js
+++ src/actions/protected-data.js
@@ -21,7 +21,7 @@ export const fetchProtectedDataError = error => ({
 export const fetchProtectedData = () => (dispatch, getState) => {
   dispatch(fetchProtectedDataRequest())
   const authToken = getState().auth.authToken;
-  return fetch(`${API_BASE_URL}/protected`, {
+  return fetch(`${API_BASE_URL}/user/profile`, {
       method: 'GET',
       headers: {
         // Provide our auth token as credentials
