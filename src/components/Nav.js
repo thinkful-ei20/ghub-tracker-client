9c87240fa5ef2e28d31279b7f22b5ddf435800fd
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-07-18 10:19:40 -0700

    added a basic link to the nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
-export class Nav extends React.Component {
+export default class Nav extends React.Component {
   render() {
     return (
       <div>
         <ul>
-          {/* <Link to="/">Login</Link> */}
+          <Link to="/">Login</Link>
         </ul>
       </div>
     )
   }
 }
 
-
-export default connect(mapStateToProps)(Nav);
--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
+import Nav from './Nav';
 
 export class HeaderBar extends React.Component {
     logOut() {
@@ -20,9 +21,10 @@ export class HeaderBar extends React.Component {
         return (
             <div className="header-bar">
                 <h1>Foo App</h1>
+                <Nav />
                 {logOutButton}
             </div>
-        );
+        )
     }
 }
 
