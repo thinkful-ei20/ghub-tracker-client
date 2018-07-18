6c7985661f68f8103d339ba72755bd0fdb4c2255
Hiram Cruz <hiram0101@gmail.com>
2018-07-18 14:51:01 -0400

    correcting merge conflict part 2

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -18,11 +18,7 @@ export default function reducer(state = initialState, action) {
       return { ...state, loading: false, error: action.error }
     case FETCH_PROTECTED_DATA_SUCCESS:
       return { ...state, loading: false, data: action.data }
-<<<<<<< HEAD
     default:
       return state
-=======
-    default: return state;
->>>>>>> a402734c255a55092a1d784af8749268382e91d9
   }
 }
