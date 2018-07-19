4233b51ded8141c5ab71983d5e279a0e31a70b11
Hiram Cruz <hiram0101@gmail.com>
2018-07-19 14:36:35 -0400

    added mock protected data

---
+++ src/mock_data/protected-data.js
@@ -0,0 +1,174 @@
+// example protected data
+export default {
+  /** PRIORITY **/
+  // personal commit breakdown
+  commits: {
+    allTime: 900,
+    lastMonth: 200,
+    lastWeek: 10,
+    yesterday: 20,
+  },
+  // private friends list
+  friends: [
+    'username1',
+    'friend2',
+    'friend7',
+  ],
+  /** NOT PRIORITY **/
+  // private friends leaderboard
+  leaderboards: {
+    allTime: [{
+        rank: 1,
+        username: 'hiram',
+        commits: 900
+      },
+      {
+        rank: 2,
+        username: 'hiram53452345',
+        commits: 800
+      },
+      {
+        rank: 3,
+        username: 'hiram14234',
+        commits: 700
+      },
+      {
+        rank: 4,
+        username: 'hiram412341',
+        commits: 600
+      },
+      {
+        rank: 5,
+        username: 'hiram1234',
+        commits: 500
+      },
+      {
+        rank: 6,
+        username: 'hiram14124',
+        commits: 450
+      },
+      {
+        rank: 7,
+        username: 'hiram321',
+        commits: 425
+      },
+    ],
+    monthly: [{
+        rank: 1,
+        username: 'hiram',
+        commits: 900
+      },
+      {
+        rank: 2,
+        username: 'hiram53452345',
+        commits: 800
+      },
+      {
+        rank: 3,
+        username: 'hiram14234',
+        commits: 700
+      },
+      {
+        rank: 4,
+        username: 'hiram412341',
+        commits: 600
+      },
+      {
+        rank: 5,
+        username: 'hiram1234',
+        commits: 500
+      },
+      {
+        rank: 6,
+        username: 'hiram14124',
+        commits: 450
+      },
+      {
+        rank: 7,
+        username: 'hiram321',
+        commits: 425
+      },
+    ],
+    weekly: [
+      {
+        rank: 1,
+        username: 'hiram',
+        commits: 900
+      },
+      {
+        rank: 2,
+        username: 'hiram53452345',
+        commits: 800
+      },
+      {
+        rank: 3,
+        username: 'hiram14234',
+        commits: 700
+      },
+      {
+        rank: 4,
+        username: 'hiram412341',
+        commits: 600
+      },
+      {
+        rank: 5,
+        username: 'hiram1234',
+        commits: 500
+      },
+      {
+        rank: 6,
+        username: 'hiram14124',
+        commits: 450
+      },
+      {
+        rank: 7,
+        username: 'hiram321',
+        commits: 425
+      },
+    ],
+    daily: [{
+        rank: 1,
+        username: 'hiram',
+        commits: 900
+      },
+      {
+        rank: 2,
+        username: 'hiram53452345',
+        commits: 800
+      },
+      {
+        rank: 3,
+        username: 'hiram14234',
+        commits: 700
+      },
+      {
+        rank: 4,
+        username: 'hiram412341',
+        commits: 600
+      },
+      {
+        rank: 5,
+        username: 'hiram1234',
+        commits: 500
+      },
+      {
+        rank: 6,
+        username: 'hiram14124',
+        commits: 450
+      },
+      {
+        rank: 7,
+        username: 'hiram321',
+        commits: 425
+      },
+    ],
+  },
+  // personal challenge data
+  challenges: {
+    currentChallenges: [],
+    pastChallenges: [],
+    victories: 10,
+    defeats: 20,
+    ties: 5,
+  }
+}
