be5bb8f0cdf8e80e7e58d3fa6f0d1a55defd070d
Hiram Cruz <hiram0101@gmail.com>
2018-07-20 13:59:49 -0400

    added mock data for public user profile

---
+++ src/mock_data/public_profile_data.js
@@ -0,0 +1,15 @@
+export default {
+  commits: {
+    allTime: 900,
+    lastMonth: 200,
+    lastWeek: 10,
+    yesterday: 20,
+  },
+  challenges: {
+    currentChallenges: [],
+    pastChallenges: [],
+    victories: 10,
+    defeats: 20,
+    ties: 5,
+  }
+}
