ec98cdb38084169e3aa77f241d884a3b1ca7b194
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-07-20 09:32:33 -0700

    added user-data component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import {fetchProtectedData} from '../actions/protected-data';
+import UserData from './user-data';
 
 export class Dashboard extends React.Component {
   componentDidMount() {
@@ -20,6 +21,7 @@ export class Dashboard extends React.Component {
         <div className="dashboard-protected-data">
           Protected data: {this.props.protectedData}
         </div>
+        <UserData />
       </div>
     );
   }
---
+++ src/components/user-data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Details from './details';
+import FriendList from './friends-list';
+
+export const UserData = () => {
+  return (
+    <div className="userData">
+      <h2>User Data</h2>
+      <Details />
+      <FriendList />
+    </div>
+  )
+}
+
+export default UserData;
