b22d4c576222b818341f96fc46682b8cf1c78546
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-07-23 12:13:26 -0700

    added login-page.css

--- src/components/login-page/login-page.js
+++ src/components/login-page/login-page.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from '../login-form';
+import './login-page.css';
 
 export function LoginPage(props) {
   // If we are logged in (which happens automatically when registration
