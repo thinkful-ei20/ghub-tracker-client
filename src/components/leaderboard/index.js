72763e2c5485eba31604cb26e5c7fce8d9e938cb
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-07-26 00:46:03 -0700

    added styling for leaderboard and rearanged leaderboard components

--- src/components/leaderboard/index.js
+++ src/components/leaderboard/index.js
@@ -38,6 +38,8 @@ import { getPublicLeaderboard } from '../../actions/leaderboard';
 import { connect } from 'react-redux';
 import requiresLogin from '../requires-login';
 
+import './leaderboard.css';
+
 export class Leaderboard extends React.Component {
 
   componentDidMount() {
@@ -45,16 +47,15 @@ export class Leaderboard extends React.Component {
   }
 
   render() {
-    // console.log('HERE',this.props)
     let leaders
     let data = this.props.leaderboard;
     if (data) {
       leaders = data.map(data => {
         return (
           <tr>
-          <td>{data.rank}</td>
-          <td>{data.username}</td>
-          <td>{data.commits}</td>
+            <td>{data.rank}</td>
+            <td>{data.username}</td>
+            <td>{data.commits}</td>
           </tr>
         )
       })
@@ -62,18 +63,69 @@ export class Leaderboard extends React.Component {
 
     return (
       <div className="leaderboard">
-        <h2>Leaderboard</h2>
-        <table>
-          <thead>
-            <th>Rank</th>
-            <th>Username</th>
-            <th>Commits</th>
-          </thead>
-          <tbody>
-            { data }
-          </tbody>
-        </table>
+        
+
+        <div className="tables">
+        <h2 className="mainTitle">Leaderboard</h2>
+          <div className="daily">
+            <h3 className="dayTitle">Daily</h3>
+            <table className="dailyTable">
+              <thead>
+                <th>Rank</th>
+                <th>Username</th>
+                <th>Commits</th>
+              </thead>
+              <tbody>
+                { data }
+              </tbody>
+            </table>
+          </div>
+
+          <div className="weekly">
+            <h3 className="weekTitle">Weekly</h3>
+            <table className="weeklyTable">
+              <thead>
+                <th>Rank</th>
+                <th>Username</th>
+                <th>Commits</th>
+              </thead>
+              <tbody>
+                { data }
+              </tbody>
+            </table>
+          </div>
+
+          <div className="monthly">
+            <h3 className="monthTitle">Monthly</h3>
+            <table className="monthlyTable">
+              <thead>
+                <th>Rank</th>
+                <th>Username</th>
+                <th>Commits</th>
+              </thead>
+              <tbody>
+                { data }
+              </tbody>
+            </table>
+          </div>
+
+          <div className="allTime">
+            <h3 className="allTitle">Alltime</h3>
+            <table className="allTable">
+              <thead>
+                <th>Rank</th>
+                <th>Username</th>
+                <th>Commits</th>
+              </thead>
+              <tbody>
+                { data }
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
+
+      
     );
   }
 }
