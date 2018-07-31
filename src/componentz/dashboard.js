7fbf15c86d1cafb9cf80d958f8ece1480e2141d5
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-31 18:58:50 -0500

    create responsive dashboard

--- src/componentz/dashboard.js
+++ src/componentz/dashboard.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 // import { fetchProtectedData } from '../actions/protected-data';
 import { fetchUserProfile } from '../actions/profile';
+import './dashboard.css';
 
 export class Dashboard extends React.Component {
   componentDidMount() {
@@ -10,15 +11,46 @@ export class Dashboard extends React.Component {
     this.props.loadProfile();
   }
 
+  // render() {
+  //   return (
+  //     <div className="dashboard">
+  //       <div className="dashboard-username">
+  //         Username: {this.props.profile.login}
+  //       </div>
+  //       <div className="dashboard-name">Name: {this.props.profile.firstname}</div>
+  //       <div className="dashboard-protected-data">
+  //         Protected data: {this.props.profile.email}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   render() {
     return (
-      <div className="dashboard">
-        <div className="dashboard-username">
-          Username: {this.props.profile.login}
+      <div class="row">
+        <div class="side">
+          <h2>About Me</h2>
+          <h5>Photo of me:</h5>
+          <div class="fakeimg" style={{ height: "200px" }}>Image</div>
+          <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+          <h3>More Text</h3>
+          <p>Lorem ipsum dolor sit ame.</p>
+          <div class="fakeimg" style={{ height: "60px" }}>Image</div><br />
+          <div class="fakeimg" style={{ height: "60px" }}>Image</div><br />
+          <div class="fakeimg" style={{ height: "60px" }}>Image</div>
         </div>
-        <div className="dashboard-name">Name: {this.props.profile.firstname}</div>
-        <div className="dashboard-protected-data">
-          Protected data: {this.props.profile.email}
+        <div class="main">
+          <h2>TITLE HEADING</h2>
+          <h5>Title description, Dec 7, 2017</h5>
+          <div class="fakeimg" style={{ height: "200px" }}>Image</div>
+          <p>Some text..</p>
+          <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          <br />
+          <h2>TITLE HEADING</h2>
+          <h5>Title description, Sep 2, 2017</h5>
+          <div class="fakeimg" style={{ height: "200px" }}>Image</div>
+          <p>Some text..</p>
+          <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
         </div>
       </div>
     );
