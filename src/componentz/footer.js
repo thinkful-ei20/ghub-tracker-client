780bc766bfa2daef025b0d6eba1e9ca2df4b4dc4
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-08-01 20:03:03 -0700

    added initial footer

--- src/componentz/footer.js
+++ src/componentz/footer.js
@@ -1,11 +1,59 @@
-import React from 'react';
+// import React from 'react';
 
+// import './footer.css';
+
+// export default (props) => {
+//   return (
+//     <footer>
+//       <p>Created by HVNJ &copy; 2018</p>
+//     </footer>
+//   );
+// }
+
+
+
+
+import React from 'react';
 import './footer.css';
 
-export default (props) => {
-  return (
-    <footer>
-      <p>Created by HVNJ &copy; 2018</p>
-    </footer>
-  );
-}
+export default function Footer() {
+	return (
+		<footer className="footer-style">
+      <p>Made with love by the MERNtalists™</p>
+      {/* <p>Contact Us</p> */}
+			<ul>
+				<li>
+					<a href="https://github.com/mdargitz">Jeffrey</a>
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Github logo'
+					/>
+				</li>
+
+				<li>
+					<a href="https://github.com/csprier">Hiram</a>
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Github logo'
+					/>
+				</li>
+
+				<li>
+					<a href="https://github.com/patersog">Neill</a>
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Github logo'
+					/>
+				</li>
+
+				<li>
+					<a href="https://github.com/Sakela17">Tharaka</a>
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Github logo'
+					/>
+				</li>
+			</ul>
+
+		</footer>
+	)};
